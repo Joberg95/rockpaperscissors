0fe3d8a3d11beb9b7d919203758c8b43a0b37837
nevroje <joakim.berglind@gmail.com>
2019-02-23 17:53:08 +0100

    added index dom content

---
+++ spec.helper.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const BrowserHelpers = require('e2e_training_wheels')
+const fs = require('fs')
+global.browser = new BrowserHelpers()
+global.expect = chai.expect;
+
+let rps = fs.readFileSync('./src/js/rock_paper_scissors.js');
+eval( rps + `\nexports.RockPaperScissors = RockPaperScissors;`)
