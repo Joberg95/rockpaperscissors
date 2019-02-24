e7624b5ea6fa9cdf36c7686b1a0ad8f6b17510e8
nevroje <joakim.berglind@gmail.com>
2019-02-24 11:52:35 +0100

    updated front end

--- spec/rock_paper_scissors.spec.js
+++ spec/rock_paper_scissors.spec.js
@@ -1,8 +1,12 @@
-import {rockPaperScissors} from '/spec.helper.js';
+const { RockPaperScissors } = require('./spec.helper')
 const mockMath = Object.create(global.Math)
 mockMath.random = () => .5
 global.Math = mockMath
   
 describe('Rock Paper Scissors', function() {
-    let rps = new rockPaperScissors() 
+    let rps = new RockPaperScissors() 
+
+    it('should return "You Win! The droids choice was scissors"', () => {
+        expect('rock').to.be.eql('"You Win! The droids choice was scissors"')
+    })
 })
