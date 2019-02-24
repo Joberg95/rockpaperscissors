8b13311396d0cbf5ba6c99dcba5d69249641fe4d
nevroje <joakim.berglind@gmail.com>
2019-02-24 17:18:29 +0100

    formatting

--- src/js/rock_paper_scissors.js
+++ src/js/rock_paper_scissors.js
@@ -10,21 +10,21 @@ return choices [randomNumber]
 
 class RockPaperScissors {
     constructor() {
-        this.play = (userChoice) => {
-            const droidChoice = getDroidChoice();
-            switch (userChoice + "-" + droidChoice) {
-                case "rock-scissors":
-                case "paper-rock":
-                case "scissors-paper":
-                    return `You Win! The droids choice was ${droidChoice}`;
-                case "rock-paper":
-                case "paper-scissors":
-                case "scissors-rock":
-                    return `You lost! The droids choice was ${droidChoice}`;
-                case "rock-rock":
-                case "paper-paper":
-                case "scissors-scissors":
-                    return `These are not the droids youre looking for! The droids choice was ${droidChoice}`;
+    this.play = (userChoice) => {
+        const droidChoice = getDroidChoice();
+        switch (userChoice + "-" + droidChoice) {
+            case "rock-scissors":
+            case "paper-rock":
+            case "scissors-paper":
+                return `You Win! The droids choice was ${droidChoice}`;
+            case "rock-paper":
+            case "paper-scissors":
+            case "scissors-rock":
+                return `You lost! The droids choice was ${droidChoice}`;
+            case "rock-rock":
+            case "paper-paper":
+            case "scissors-scissors":
+                return `These are not the droids youre looking for! The droids choice was ${droidChoice}`;
             }
         };
     }
